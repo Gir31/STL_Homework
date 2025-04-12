6b46864cdd2d380b5e33ad50363272973e13a38c
Gir31_PC <rpg2657@gmail.com>
2025-04-12 20:33:26 +0900

    파일 읽기

--- Homework/Homework.cpp
+++ Homework/Homework.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include "PLAYER.h"
 
 int main()
 {
+	std::cout << "[파일]2025 STL 과제 파일 - 2021184002 읽기" << std::endl;
+	std::ifstream in{ "2025 STL 과제 파일 - 2021184002", std::ios::binary };
+	if (not in) {
+		std::cout << "[파일]	파일 읽기 실패" << std::endl;
+		return 2021184002;
+	}
 
 }
