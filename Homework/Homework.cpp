91884edbf9e1739ef5c6599db443fb0c2262046e
Gir31_PC <rpg2657@gmail.com>
2025-04-12 20:47:49 +0900

    순서대로 읽어오는 코드
    [문제]
    1. 예외 발생  throw std::bad_alloc{};
    2. 출력되는 결과를 확인했을때 score까지는 잘 출력이 됐지만 id부터 이상한 값이 출력되기 시작
    3. unique_ptr은 읽어오지도 못함

--- Homework/Homework.cpp
+++ Homework/Homework.cpp
@@ -15,5 +15,44 @@ int main()
 	}
 	std::cout << "[파일]	파일 읽기 성공" << std::endl;
 
-	in.read((char*)players.data(), players.size() * sizeof(Player));
+	std::string name;
+	int score;
+	size_t id;
+	size_t num;
+	std::unique_ptr<char[]> p{};
+
+
+	int a{ 0 };
+	while (true) {
+		switch (a % 5) {
+		case 0:
+			std::cout << a / 5 << "번째 객체" << std::endl;
+			in.read((char*)&name, sizeof(std::string));
+			std::cout << "name : " << name << std::endl;
+			break;
+		case 1:
+			in.read((char*)&score, sizeof(int));
+			std::cout << "score : " << score << std::endl;
+			break;
+		case 2:
+			in.read((char*)&id, sizeof(size_t));
+			std::cout << "id : " << id << std::endl;
+			break;
+		case 3:
+			in.read((char*)&num, sizeof(size_t));
+			std::cout << "num : " << num << std::endl;
+			break;
+		case 4:
+			p.reset();
+			p = std::make_unique<char[]>(num);
+			in.read(p.get(), num);
+
+			for (int i = 0; i < num; ++i)
+				std::cout << p[i];
+
+			std::cout << std::endl;
+			break;
+		}
+		++a;
+	}
 }
